e814ad4d6f37404d67ef1cd1a176456e58cba359
Michael Amiro <odindo.michael@gmail.com>
2021-08-03 10:16:54 +0300

    Functional state

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+// An object literal
+var app = {
+	init: function () {
+		app.functionOne()
+	},
+	functionOne: function () {},
+	scrollTop: function () {
+		window.scrollTo({top: 0, behavior: "smooth"})
+	},
+}
+;(function () {
+	// your page initialization code here
+	// the DOM will be available here
+	app.init()
+})()
+
+var ctx = document.getElementById("myChart").getContext("2d")
+var myChart = new Chart(ctx, {
+	type: "line",
+	data: {
+		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+		datasets: [
+			{
+				data: [0, 14, 10, 24, 22, 45, 50],
+				fill: true,
+				backgroundColor: "rgba(75, 192, 192, 0.2)",
+				borderColor: "rgb(75, 192, 192)",
+				tension: 0.1,
+			},
+		],
+	},
+	options: {
+		maintainAspectRatio: true,
+		scales: {
+			x: {
+				grid: {
+					display: false,
+				},
+			},
+			y: {
+				beginAtZero: false,
+				grid: {
+					display: true,
+				},
+			},
+		},
+		plugins: {
+			legend: {
+				display: false,
+				labels: {
+					color: "rgb(255, 99, 132)",
+				},
+			},
+		},
+	},
+})
